726cfea78c0d53d63a66c78c0f903718bae89180
shivanandgante470 <shivanandgante470@gmail.com>
2022-01-30 11:53:54 +0530

    Added main.tf file

---
+++ main.tf
@@ -0,0 +1,40 @@
+# ARM Provider Block
+provider "azurerm" {
+  features{}
+}
+
+# Terraform Backend Configuration Block Pre-Created
+terraform {
+  backend "azurerm"{
+      
+     resource_group_name = "rg-storageaccount" 
+     storage_account_name = "azurestorageaccount"
+     container_name = "copystoragetfstate"
+     key = "copyterraform.tfstate"
+  }
+}
+
+
+# Resource Group Creation Block
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# Create Storage Account-1
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname1"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
+
+# Create Storage Account-2
+resource "azurerm_storage_account" "example2" {
+  name                     = "storageaccountname2"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
